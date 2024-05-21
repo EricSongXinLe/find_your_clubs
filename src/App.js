0e6d01d48e098472d80e5643a6b4101178c00119
EricS <song_xinle@163.com>
2024-05-20 21:14:03 -0700

    added Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import * as React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 import Home from './Home';
 
-/*Now there are bugs in giving the whole array as a parameter. CHECK that later!*/
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+  {
+    path: "/signin",
+    element: <div>Sign In</div>,
+  }
+]);
+
 export default function App() {
   return (
-    <Home/>
+    <RouterProvider router={router} />
   );
 }
