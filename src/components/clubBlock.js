80f6a7b70e8e77a5b43b29b4278cf3a44cbc3475
Yike <shiyikeInterSR4@outlook.com>
2024-05-27 03:40:40 +0800

    Last Commit. Branch is now bullshit
    Fuck! There are bugs! Fuck! Fuck Fuck!!!!!

--- src/components/clubBlock.js
+++ src/components/clubBlock.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
-const ClubBlock = ({ image, title, description }) => {
+const ClubBlock = ({ image, title, description, id }) => {
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push(`/club/${id}`);
+  };
+
     return (
-        <div className="club-block">
+        <div className="club-block" onClick={handleClick}>
             <img src={image} id="club-image" />
             <div className="club-details">
                 <h2 id="club-title">{title}</h2>
