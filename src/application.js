5f88cad184552087dffb76a339c05985795ee886
Samaritan001 <999000111@sina.com>
2024-05-26 17:22:40 -0700

    Change default function to Application. Refine interface display

--- src/application.js
+++ src/application.js
@@ -3,11 +3,17 @@ import {useState} from "react";
 const questionList = ["name", "email", "gender", "birthday"];
 
 function saveData() {
+  // var fs = require('fs');
+  // fs.appendFile("testOutput.txt", "Hello World");
+  // console.log(fs.readFile("testInput.txt"));
+
+
+  // if no selection
   let selection = document.getElementById("Select1").value;
   if (selection === "select") {
-    document.getElementById("q4Text").style.color='#FF5733';
-    document.getElementById("q4NA").style.color='#FF5733';
-    document.getElementById("q4NA").innerHTML = "*Required";
+    let select_q4 = document.getElementById("q4Text");
+    select_q4.style.color='#FF5733';
+    select_q4.innerHTML = select_q4.innerHTML + " Required";
   }
 
   const path = "log.txt";
@@ -15,70 +21,88 @@ function saveData() {
   // let file = new File(path);
   let rnd = questionList.length;
   for (let i = 0; i < rnd; i++) {
-    console.log(document.getElementById(questionList[i]).value);
-    //file.writeln(document.getElementById(questionList[i]).value);
-    //fs.writeFile(path, document.getElementById(questionList[i]).value);
+    let text_box = document.getElementById(questionList[i]);
+    console.log(text_box.id.substring(0, 9));
+    if (text_box.id.substring(0, 9) == "Selection")
+    {
+      // selection empty case
+      console.log("selection");
+
+    }
+    else if (text_box.id.substring(0, 4) in questionList)
+    {
+      console.log("text");
+    }
+
+    console.log(text_box.value);
+    console.log(text_box.id);
+    // write data into json file
   }
   document.getElementById('texto').innerHTML = "Happy Birthday".concat(
     " ", document.getElementById(questionList[0]).value);
 }
 
+/*
 function createInputs() {
   let inputs = [];
   for (let i = 0; i < questionList.legth; i++) {
-    let a = <input type="text" name="" id={questionList[i]} placeholder="Enter your name here"></input>
-    inputs.push(a);
+    // let a = <input type="text" name="" id={questionList[i]} placeholder="Enter your name here"></input>
+    inputs.push("Alala");
+    console.log(inputs[0])
   }
   return inputs;
 }
+*/
 
 
-export default function main() {
+export default function Application() {
   
-  /*
-  document.querySelector('#myform').addEventListener('input', (Event)=>{
-    console.log(Event.target.value);
-  })
-  */
-  const inputs = createInputs();
+  // let inputs = createInputs();
+  // console.log(inputs[0])
 
-  let a = <input type="text" name="" id="myform" placeholder="Enter your name here"></input>;
+  let block_list = [];
+  let rnd = questionList.length;
+  for (let i = 0; i < rnd; i++) {
+    block_list.push(<input type="text" name="" id={questionList[i]} placeholder={"Enter your " + questionList[i] + " here"}></input>);
+  }
+  // let a = <input type="text" name="" id="myform" placeholder="Enter your name here"></input>;
+  // let sh = String(inputs[0]);
+
+
+  
 
   return (
   <>
+    <div className="row">
+      <div className="text" id="q1Text">What is your name?*</div>
+      {block_list[0]}
+    </div>
+    <div className="row">
+      <div className="text" id="q2Text">What is your Email?*</div>
+      {block_list[1]}
+    </div>
+    <div className="row">
+      <div className="text" id="q3Text">What is your Gender?*</div>
+      {block_list[2]}
+    </div>
     
-    <div className="row">
-      <div className="text" id="q1Text">What is your name? </div>
-      <input type="text" name="" id={questionList[0]} placeholder="Enter your name here"></input>
-    </div>
-    <div className="row">
-      <div className="text" id="q2Text">What is your Email? </div>
-      <input type="text" name="" id={questionList[1]} placeholder="Enter your Email here"></input>
-    </div>
-    <div className="row">
-      <div className="text" id="q3Text">What is your Gender? </div>
-      <input type="text" name="" id={questionList[2]} placeholder="Enter your gender here"></input>
-    </div>
-    
-    <div className="text" id="q4Text">What is your Year of Graduation?</div><p id="q4NA"></p>
+    <div className="text" id="q4Text">What is your Year of Graduation?*</div><p id="q4NA"></p>
     <select name="drop1" id="Select1">
       <option value="select">select</option>
       <option value="2024">2024</option>
       <option value="2025">2025</option>
       <option value="2026">2026</option>
       <option value="2027">2027</option>
-    </select>
+    </select><p></p>
     
     <div className="row">
-      <div className="text" id="q5Text">What is your Birthday? </div>
-      <input type="text" name="" id={questionList[3]} placeholder="Enter your gender here"></input>
+      <div className="text" id="q5Text">What is your Birthday?*</div>
+      {block_list[3]}
     </div>
     
-    <button id="button" onClick={saveData}>click me</button>
-    <button id="button">don't click me</button>
+    <button id="button" onClick={saveData}>Click Me</button>
+    <button id="button">Don't Click Me</button>
     <p id="texto"></p>
-
-
   </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
-import App from "./App";
+import Application from "./Application.js";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <App />
+    <Application />
   </StrictMode>
 );
