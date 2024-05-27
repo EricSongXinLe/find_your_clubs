e5092b77e0f0a9a8a0685c30a9f1bc29d7a9ce7d
EricS <song_xinle@163.com>
2024-05-26 20:30:04 -0700

    update login

--- src/Signup.js
+++ src/Signup.js
@@ -48,20 +48,20 @@ function Signup() {
         <link rel="stylesheet" href="signup.css" />
             <h1>Create an Account</h1>
             <form action="POST">
-                <input type="text" onChange={(e) => {setUsername(e.target.value)}} placeholder="Username" className="input" />
-                <input type="email" onChange={(e) => { setEmail(e.target.value) }} placeholder="Email"  />
-                <input type="password" onChange={(e) => { setPassword(e.target.value) }} placeholder="Password" />
+                <input class="textInput" type="text" onChange={(e) => {setUsername(e.target.value)}} placeholder="Username" />
+                <input class="textInput" type="email" onChange={(e) => { setEmail(e.target.value) }} placeholder="Email"  />
+                <input class="textInput" type="password" onChange={(e) => { setPassword(e.target.value) }} placeholder="Password" />
                 <br/>
                 <label>Are you a club leader?</label>
                 <label class="switch">
-                    <input type="checkbox" onChange={(e) => {
+                    <input class="checkbox" type="checkbox" onChange={(e) => {
                         setUserIsClubLeader(e.target.checked)}}/> 
                     <span class="slider round"></span>
                 </label>
                 <br/>
-                <input type="submit" onClick={submit} />
+                <input class= "submitButton" type="submit" onClick={submit} />
             </form>
-            <Link to="/">Back to Login</Link>
+            <Link to="/login">Back to Login</Link>
         </div>
     )
 }
