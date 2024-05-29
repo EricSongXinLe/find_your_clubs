14e41e42cace9be8f34c94ce87a8aca9ce1b850d
Samaritan001 <999000111@sina.com>
2024-05-29 01:10:47 -0700

    Complete the question fetching interface, waiting to be tested. Send answers to databse in two lists, answers for general questions and supplementary questions.

--- src/application.js
+++ src/application.js
@@ -20,7 +20,91 @@ const selection_num = selections.length;
 
 // supplementary questions
 let supplementaries = ["A unique question*", "Another unique question*"];
+supplementaries = getCreation();
 const supplementary_num = supplementaries.length;
+
+
+function Apply() {
+
+  let titles = [];
+  let inboxes = [];
+  
+  for (let i = 0; i < input_num; i++) {
+    titles.push(<p className="text" id={"iTitle" + String(i)}>{input_titles[i]}</p>)
+    inboxes.push(<input type="text" id={inputs[i]} placeholder={input_hints[i]}></input>);
+  }
+
+  // let supplementaries = db.user.find({clubname: {clubname}}, {supplementaries: 1});
+  
+  for (let i = 0; i < 3; i++)
+  {
+    // input_titles.push("Supplementary Question " + String(i+1) + "*");
+    if (i >= supplementary_num)
+    {
+      // no question provided
+      titles.push(<a></a>);
+      inboxes.push(<a></a>);
+    }
+    else
+    {
+      titles.push(<p className="text" id={"sTitle" + String(i)}>{supplementaries[i]}</p>)
+      inboxes.push(<input type="text" id={"supplementary" + String(i)} placeholder="Enter your answer"></input>);
+    }
+  }
+
+  document.body.style.overflow = "visible";
+
+  return (
+  <>
+    <div className="row">
+      {titles[0]}
+      {inboxes[0]}
+    </div>
+    <div className="row">
+      {titles[1]}
+      {inboxes[1]}
+    </div>
+    <div className="row">
+      {titles[2]}
+      {inboxes[2]}
+    </div>
+    
+    <div className="row">
+      <a name="text" id="sTitle">{selection_titles[0]}</a><a> </a>
+      <select name="drop1" id="Select1">
+        <option value="select">select</option>
+        <option value="2024">2024</option>  
+        <option value="2025">2025</option>
+        <option value="2026">2026</option>
+        <option value="2027">2027</option>
+      </select>
+    </div>
+
+    <div className="row">
+      {titles[3]}
+      {inboxes[3]}
+    </div>
+
+    <div className="row">
+      {titles[4]}
+      {inboxes[4]}
+    </div>
+
+    <div className="row">
+      {titles[5]}
+      {inboxes[5]}
+    </div>
+
+    <div className="row">
+      {titles[6]}
+      {inboxes[6]}
+    </div>
+    
+    <button id="submit application button" onClick={saveAnswer}>Submit</button>
+    <p id="texto"></p>
+  </>
+  );
+}
 
 function saveAnswer() {
   let finished = true;
@@ -79,36 +163,35 @@ function saveAnswer() {
     }
   }
 
-
   // don't process if unfinished
   if (!finished)
     return;
 
-  let answer_list = [];
-  let supplementary_answer_list = [];
+  let answers = [];
+  let supplementary_answers = [];
 
   // add input questions' answers to list
   for (let i = 0; i < input_num; i++)
   {
     let text_box = document.getElementById(inputs[i]);
-    answer_list.push(text_box.value);
+    answers.push(text_box.value);
   }
 
   // add selection questions' answers to list
   for (let i = 0; i < selection_num; i++)
   {
     let selection = document.getElementById("Select1");
-    answer_list.push(selection.value);
+    answers.push(selection.value);
   }
 
   // add supplementary questions' answers to list
   for (let i = 0; i < supplementary_num; i++)
   {
     let text_box = document.getElementById("supplementary" + String(i));
-    supplementary_answer_list.push(text_box.value);
+    supplementary_answers.push(text_box.value);
   }
 
-  postAnswer(answer_list, supplementary_answer_list); // send answers to backend database
+  postAnswer(answers, supplementary_answers); // send answers to backend database
 
   // Happy Birthday
   document.getElementById('texto').innerHTML = "Happy Birthday".concat(
@@ -116,93 +199,12 @@ function saveAnswer() {
 
 }
 
-function Apply() {
 
-  let title_list = [];
-  let inbox_list = [];
-  
-  for (let i = 0; i < input_num; i++) {
-    title_list.push(<p className="text" id={"iTitle" + String(i)}>{input_titles[i]}</p>)
-    inbox_list.push(<input type="text" id={inputs[i]} placeholder={input_hints[i]}></input>);
-  }
-
-  // let supplementaries = db.user.find({clubname: {clubname}}, {supplementaries: 1});
-  
-  for (let i = 0; i < 3; i++)
-  {
-    // input_titles.push("Supplementary Question " + String(i+1) + "*");
-    if (i >= supplementary_num)
-    {
-      // no question provided
-      title_list.push(<a></a>);
-      inbox_list.push(<a></a>);
-    }
-    else
-    {
-      title_list.push(<p className="text" id={"sTitle" + String(i)}>{supplementaries[i]}</p>)
-      inbox_list.push(<input type="text" id={"supplementary" + String(i)} placeholder="Enter your answer"></input>);
-    }
-  }
-
-  document.body.style.overflow = "visible";
-
-  return (
-  <>
-    <div className="row">
-      {title_list[0]}
-      {inbox_list[0]}
-    </div>
-    <div className="row">
-      {title_list[1]}
-      {inbox_list[1]}
-    </div>
-    <div className="row">
-      {title_list[2]}
-      {inbox_list[2]}
-    </div>
-    
-    <div className="row">
-      <a name="text" id="sTitle">{selection_titles[0]}</a><a> </a>
-      <select name="drop1" id="Select1">
-        <option value="select">select</option>
-        <option value="2024">2024</option>  
-        <option value="2025">2025</option>
-        <option value="2026">2026</option>
-        <option value="2027">2027</option>
-      </select>
-    </div>
-
-    <div className="row">
-      {title_list[3]}
-      {inbox_list[3]}
-    </div>
-
-    <div className="row">
-      {title_list[4]}
-      {inbox_list[4]}
-    </div>
-
-    <div className="row">
-      {title_list[5]}
-      {inbox_list[5]}
-    </div>
-
-    <div className="row">
-      {title_list[6]}
-      {inbox_list[6]}
-    </div>
-    
-    <button id="submit application button" onClick={saveAnswer}>Submit</button>
-    <p id="texto"></p>
-  </>
-  );
-}
-
-async function postAnswer(answer_list, supplementary_answer_list)
+async function postAnswer(answers, supplementary_answers)
 {
   try{  
-    await axios.post("http://localhost:8000/apply",{
-        answer_list, supplementary_answer_list
+    await axios.post("http://localhost:8000/application",{
+        answers, supplementary_answers
     })
     .then(res=>{
         if(res.data=="exist"){
@@ -222,7 +224,7 @@ async function postAnswer(answer_list, supplementary_answer_list)
   }
 }
 
-/*
+
 async function getCreation(e){
   e.preventDefault();
 
@@ -233,27 +235,26 @@ async function getCreation(e){
       })
       .then(res=>{
           if(res.data){
-              alert("Club found")
+            alert("Club found")
+            return res.data["supplementary_questions"];
           }
           else{
-              alert("Club not found")
+            alert("Club not found")
           }
       })
       .catch(e=>{
-          alert("An error occurred")
-          console.log(e);
+        alert("An error occurred")
+        console.log(e);
       })
 
   }
   catch(e){
-      console.log(e);
+    console.log(e);
 
   }
 
-  res
-
 }
-*/
+
 
 
 export default Apply;
