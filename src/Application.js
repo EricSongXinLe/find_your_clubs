23978256c052e0c34414ee1aaf805c3212394a0d
Bree Chen <bree_chen2005@126.com>
2024-05-29 08:38:44 +0800

    Create application database and link with club application page

--- src/Application.js
+++ src/Application.js
@@ -1,47 +1,87 @@
 import {useState} from "react";
+import axios from "axios"
 
-const questionList = ["name", "email", "gender", "birthday"];
+const inputList = ["name", "email", "gender", "birthday"];
+const input_num = inputList.length;
+const inputHintList = [];
+const inputTitleList = [];
+for (let i = 0; i < input_num; i++)
+{
+  inputHintList.push("Enter your " + inputList[i] + " here");
+  inputTitleList.push("What is your " + inputList[i] + "?*");
+}
+
+const selectionTitleList = [];
+selectionTitleList.push("What is your Year of Graduation?*");
+const selection_num = selectionTitleList.length;
+
 
 function saveData() {
-  let selection = document.getElementById("Select1").value;
-  if (selection === "select") {
-    document.getElementById("q4Text").style.color='#FF5733';
-    document.getElementById("q4NA").style.color='#FF5733';
-    document.getElementById("q4NA").innerHTML = "*Required";
+  // var fs = require('fs');
+  // fs.appendFile("testOutput.txt", "Hello World");
+  // console.log(fs.readFile("testInput.txt"));
+
+
+  let finished = true;
+
+  for (let i = 0; i < input_num; i++)
+    {
+      let text_box = document.getElementById(inputList[i]);
+      let question = document.getElementById("iTitle" + String(i));
+      if (text_box.value == "") // if empty
+      {
+        question.innerHTML = inputTitleList[i] + " Required";
+        question.style.color = '#FF5733';
+        finished = false;
+      }
+      else if (question.innerHTML != inputTitleList[i]) // revert to normal
+      {
+        question.style.color = '#000000';
+        question.innerHTML = inputTitleList[i];
+      }
+    }
+
+  for (let i = 0; i < selection_num; i++)
+  {
+    let selection = document.getElementById("Select1");
+    let select = document.getElementById("sTitle");
+    if (selection.value == "select") // if no selection
+    {
+      select.style.color='#FF5733';
+      select.innerHTML = selectionTitleList[i] + " Required";
+      finished = false;
+    }
+    else if (select.innerHTML != selectionTitleList[i]) // revert to normal
+    {
+      select.style.color='#000000';
+      select.innerHTML = selectionTitleList[i];
+    }
   }
 
-  const path = "log.txt";
-  //const fs = require('fs');
-  // let file = new File(path);
-  let rnd = questionList.length;
-  for (let i = 0; i < rnd; i++) {
-    console.log(document.getElementById(questionList[i]).value);
-    //file.writeln(document.getElementById(questionList[i]).value);
-    //fs.writeFile(path, document.getElementById(questionList[i]).value);
+  if (!finished)
+    return;
+
+  let answer_list = [];
+
+  for (let i = 0; i < input_num; i++)
+  {
+    let text_box = document.getElementById(inputList[i]);
+    answer_list.push(text_box.value);
   }
-<<<<<<< Updated upstream
-=======
 
   for (let i = 0; i < selection_num; i++)
   {
     let selection = document.getElementById("Select1");
     answer_list.push(selection.value);
   }
-  console.log(answer_list)
+  
   postData(answer_list);
 
->>>>>>> Stashed changes
   document.getElementById('texto').innerHTML = "Happy Birthday".concat(
-    " ", document.getElementById(questionList[0]).value);
+    " ", document.getElementById(inputList[0]).value);
+
 }
 
-<<<<<<< Updated upstream
-function createInputs() {
-  let inputs = [];
-  for (let i = 0; i < questionList.legth; i++) {
-    let a = <input type="text" name="" id={questionList[i]} placeholder="Enter your name here"></input>
-    inputs.push(a);
-=======
 async function postData(answer_list)
 {
   try{  
@@ -50,7 +90,7 @@ async function postData(answer_list)
     })
     .then(res=>{
         if(res.data=="exist"){
-            alert("You've already submitted application")
+            alert("You've already submitted the application")
         }
         else if(res.data=="added"){
             alert("Application submitted successfully")
@@ -63,58 +103,62 @@ async function postData(answer_list)
   }
   catch(e){
       console.log(e);
->>>>>>> Stashed changes
   }
-  return inputs;
 }
 
 
-export default function Application() {
-  
-  /*
-  document.querySelector('#myform').addEventListener('input', (Event)=>{
-    console.log(Event.target.value);
-  })
-  */
-  const inputs = createInputs();
 
-  let a = <input type="text" name="" id="myform" placeholder="Enter your name here"></input>;
+function Application() {
+  
+  // let inputs = createInputs();
+  // console.log(inputs[0])
+
+  let block_list = [];
+  let title_list = [];
+  
+  for (let i = 0; i < input_num; i++) {
+    block_list.push(<input type="text" name="" id={inputList[i]} placeholder={inputHintList[i]}></input>);
+    title_list.push(<div className="text" id={"iTitle" + String(i)}>{inputTitleList[i]}</div>)
+  }
+  // let a = <input type="text" name="" id="myform" placeholder="Enter your name here"></input>;
+  // let sh = String(inputs[0]);
+
+
+  
 
   return (
   <>
+    <div className="row">
+      {title_list[0]}
+      {block_list[0]}
+    </div>
+    <div className="row">
+      {title_list[1]}
+      {block_list[1]}
+    </div>
+    <div className="row">
+      {title_list[2]}
+      {block_list[2]}
+    </div>
     
-    <div className="row">
-      <div className="text" id="q1Text">What is your name? </div>
-      <input type="text" name="" id={questionList[0]} placeholder="Enter your name here"></input>
-    </div>
-    <div className="row">
-      <div className="text" id="q2Text">What is your Email? </div>
-      <input type="text" name="" id={questionList[1]} placeholder="Enter your Email here"></input>
-    </div>
-    <div className="row">
-      <div className="text" id="q3Text">What is your Gender? </div>
-      <input type="text" name="" id={questionList[2]} placeholder="Enter your gender here"></input>
-    </div>
-    
-    <div className="text" id="q4Text">What is your Year of Graduation?</div><p id="q4NA"></p>
+    <div className="text" id="sTitle">{selectionTitleList[0]}</div><p></p>
     <select name="drop1" id="Select1">
       <option value="select">select</option>
       <option value="2024">2024</option>
       <option value="2025">2025</option>
       <option value="2026">2026</option>
       <option value="2027">2027</option>
-    </select>
+    </select><p></p>
     
     <div className="row">
-      <div className="text" id="q5Text">What is your Birthday? </div>
-      <input type="text" name="" id={questionList[3]} placeholder="Enter your gender here"></input>
+      {title_list[3]}
+      {block_list[3]}
     </div>
     
-    <button id="button" onClick={saveData}>click me</button>
-    <button id="button">don't click me</button>
+    <button id="button" onClick={saveData}>Click Me</button>
     <p id="texto"></p>
-
-
   </>
   );
 }
+
+export default Application
