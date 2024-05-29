3a2162f436d160032f2c2685adc9ae9b3f5273fd
Bree Chen <bree_chen2005@126.com>
2024-05-29 15:11:33 +0800

    Fetch club data by searching by

--- server/app.js
+++ server/app.js
@@ -135,6 +135,32 @@ app.post("/application", async(req, res)=>{
         res.json("fail") }
 })
 
+
+app.get('/search', async(req, res)=>{
+    const clubname = req.query.clubname;
+    console.log("AHAHAHAH")
+    // console.log(clubname)
+    try{
+        const clubdata = await club_collection.findOne({clubname:clubname})
+        console.log("BBBBB")
+        console.log(clubdata)
+        // await club_collection.findOne({clubname:clubname})
+        if (clubdata) {
+            res.json(clubdata)
+            console.log("CCCCC")
+            console.log(clubdata)
+            console.log("DDDDD")
+        }
+        else{
+            res.json("fail")
+        }
+    }
+    catch(e){
+        res.json("fail")
+    }
+})
+
+
 app.post("/addclub",async(req,res)=>{
     const{clubname, foundingTime, tagsList, clubdescription, requirement, cs, math, physics, economics, ds, me} = req.body
 
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,27 +1,68 @@
 import React, { useState } from 'react';
+import axios from "axios"
+
+
+    // const handleInputChange = (event) => {
+        // setQuery(event.target.value);
+
+    // };
+
 
 const SearchBar = () => {
-    const [query, setQuery] = useState('');
+const [search, setSearch] = useState('')
+const [clubinfo, setClubInfo] = useState('')
 
-    const handleInputChange = (event) => {
-        setQuery(event.target.value);
+const [query, setQuery] = useState('');
+
+async function handleInputChange(e)  {
+        e.preventDefault();
+        try{
+            
+            // console.log(search)
+            await axios.get('http://localhost:8000/search', { params: { clubname: search } })
+            .then(
+                res=>{
+                    if(res.data=="fail"){
+                        alert("Club not exist!")
+                    }
+                    else{
+                        setClubInfo(res.data);
+                        console.log(clubinfo["clubdescription"])
+                        // console.log(clubinfo.clubname)
+                    }
+                }
+            ).catch((e)=>
+                console.log(e)
+            )
+            
+                
+                
+        }
+        catch(e){
+            console.log(e);
+        }
     };
 
     const handleSearch = () => {
         // Perform search logic here using the query
         alert('Searching for: '+query);
     };
-
+    
     return (
+        
         <div className='searchBar'>
+        
             <input
                 id="searchInput"
                 type="text"
                 placeholder="Search..."
-                value={query}
-                onChange={handleInputChange}
+                // value={query}
+                // onChange={handleInputChange}
+                onChange={(e) => { setSearch(e.target.value) }}
             />
-            <img id="searchButton"src={require("../images/search.gif")} onClick={handleSearch}/>
+            <img id="searchButton"src={require("../images/search.gif")} onClick={handleInputChange}
+            // onClick={handleSearch}
+            />
         </div>
     );
 };
