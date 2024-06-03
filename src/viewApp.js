ff491689721c6e2cc51427876af84b51a8a97efe
Samaritan001 <999000111@sina.com>
2024-06-03 04:10:32 -0700

    CONNECT VIEWAPP TO DATABASE AND SHOW THE DATA SUCCESSFULLY FINALLYgit add src/viewApp.jsgit add src/viewApp.jsgit add src/viewApp.jsgit add src/viewApp.jsgit add src/viewApp.js

--- src/viewApp.js
+++ src/viewApp.js
@@ -3,56 +3,59 @@ import axios from "axios"
 import './viewApp.css'
 
 
-async function viewApp() {
+function ViewApp() {
     document.body.style.overflow = "visible";
     const [applicant, setApplicant] = useState("Default");
     const [applicant_list, setApplicantList] = useState(["Default"])
     const [answer_dict, setAnswerDict] = useState({"Default":["Default Answer"]})
 
     const [answers, setAnswer] = useState([]);
-    if (answers.length == 0)
-        await getAnswer("xdf");
+    useEffect(()=>{
+        if (answers.length == 0)
+            getAnswer("xdf");
+
+    })
 
     async function getAnswer(clubName) {
-        return new Promise((resolve, reject) => {
-            try {
-                console.log("getting")
-                axios.get("http://localhost:8000/fetch_answer", {
-                    params: { clubName: clubName }
+        try {
+            console.log("getting")
+            await axios.get("http://localhost:8000/fetch_answer", {
+                params: { clubName: clubName }
+            })
+                .then(res => {
+                    if (res.data) {
+                        // alert("Club found")
+                        setAnswer(res.data)
+                        console.log("answers found", res.data);
+                        return res.data;
+                    }
+                    else {
+                        alert("Club not found")
+                    }
                 })
-                    .then(res => {
-                        if (res.data) {
-                            alert("Club found")
-                            setAnswer(res.data)
-                            console.log("answers found", res.data);
-                            return res.data;
-                        }
-                        else {
-                            alert("Club not found")
-                        }
-                    })
-                    .catch(e => {
-                        alert("An error occurred")
-                        console.log(e);
-                    })
-    
-            }
-            catch (e) {
-    
-            }
-        });
-        
-    }
+                .catch(e => {
+                    alert("An error occurred")
+                    console.log(e);
+                })
 
+        }
+        catch (e) {
+
+        }
+    }
     // supplementary questions  
     const [supplementaries, setData] = useState([]);
-    await getQuestion("xdf")
+    useEffect(()=>{
+        if (supplementaries.length === 0)
+            getQuestion("xdf")
+    })
+    
 
     async function getQuestion(clubName) {
-        return new Promise((resolve, reject) => {
+
         try {
 
-            axios.get("http://localhost:8000/fetch_question", {
+            await axios.get("http://localhost:8000/fetch_question", {
                 params: { clubName: clubName }
             })
                 .then(res => {
@@ -75,10 +78,9 @@ async function viewApp() {
         catch (e) {
 
         }
-        });
     }
-
-
+    // console.log(supplementaries);
+    
     // build answer dictionary
     let temp_applicant_list = [];
     let temp_answer_dict = {};
@@ -87,24 +89,35 @@ async function viewApp() {
         temp_applicant_list.push(answers[i]["username"]);
         temp_answer_dict[answers[i]["username"]] = answers[i]["answers"];
     }
-    console.log("upper", applicant_list,answer_dict, answers);
-    setApplicantList(temp_applicant_list);
-    setAnswerDict(temp_answer_dict);
+    // console.log("upper", applicant_list,answer_dict, answers);
 
-
-    setApplicant(applicant_list[0]);
+    let empty_flag = true;
+    useEffect(() => {
+        if (temp_applicant_list.length == 0)
+            return;
+        setApplicantList(temp_applicant_list);
+        setAnswerDict(temp_answer_dict);
+        setApplicant(temp_applicant_list[0]);
+    }, [answers, supplementaries]);
+    /*
+    console.log("try", applicant, answer_dict, answer_dict[applicant]);
+    answer_dict[applicant].map((answer, index) => (
+        console.log(index, answer)
+    ))
+    */
+    
 
     // build question dictionary
     const general_questions = ["name", "email", "gender", "birthday", "yog"];
     const question_list = general_questions.concat(supplementaries);
 
-    
+
     function refresh() {
         if (document.getElementById("applicant_selection"))
             setApplicant(document.getElementById("applicant_selection").value);
     }
 
-    console.log("lower", applicant_list,answer_dict);
+    // console.log("lower", applicant_list,answer_dict);
     return (
         <>
             <select id="applicant_selection" onChange={refresh}>
@@ -112,7 +125,7 @@ async function viewApp() {
                     <option key={name}>{name}</option>
                 ))}
             </select><p></p>
-
+            
             {answer_dict[applicant].map((answer, index) => (
                 <>
                     <div>
@@ -126,31 +139,27 @@ async function viewApp() {
                     </div>
                 </>
             ))}
+            
         </>
     );
-}
-
-function ViewApp()
-{
-    return viewApp();
+    
+    return <a>Loading</a>
 }
 
 
 
 export default ViewApp;
 
-
-
 /*
-{supplementary[applicant].map((pair) => (
+{answer_dict[applicant].map((answer, index) => (
     <>
         <div>
             <h2 className="Title">Question: </h2>
-            <a className="Content">{pair[0]}</a>
+            <a className="Content">{question_list[index]}</a>
         </div>
         <div>
             <h2 className="Title">Answer: </h2>
-            <a className="Content">{pair[1]}</a>
+            <a className="Content">{answer}</a>
             <p></p>
         </div>
     </>
